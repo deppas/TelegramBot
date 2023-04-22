82640c783a4437aaf5ecffbb07061617caa70340
dmitijorsadskij <deppasss@mail.ru>
2023-04-22 14:44:38 +0800

    Add

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 TOKEN = '5904580713:AAGj_MRNx0OuHhvsFrtdaRGuE3sLAgbJNYk'
-CURRENCY_API_KEY = 'kv9x5uAVSg09dKyU8W55aVIVHeR9zjjZ'
-CURRENCY_API_BASE_URL = 'https://api.apilayer.com/exchangerates_data/latest?symbols=symbols&base=base'
+
 
 
